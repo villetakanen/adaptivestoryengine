0a8a31aa2a07e670549424263fb50cbcbc2b80f6
Ville Takanen <ville.takanen@foodie.fm>
2019-07-05 14:50:48 +0300

    Moved placeholder json from storyboard to a asset file. And did some small updates to book json structure.

--- src/components/StoryBoard.vue
+++ src/components/StoryBoard.vue
@@ -4,15 +4,15 @@
             <v-flex>
                 <v-card>
                     <v-card-title primary-title>
-                        <h1>{{placeholder_book.chapters[cursor].title}}</h1>  
+                        <h1>{{book[bookmark].title}}</h1>  
                     </v-card-title>
                     <v-card-text>
-                        <p>{{placeholder_book.chapters[cursor].text}}</p>
+                        <p>{{book[bookmark].text}}</p>
                     </v-card-text>
                     <v-card-actions>
                         <v-btn 
-                            @click="cursor=action.target"
-                            v-for="action in placeholder_book.chapters[cursor].actions" v-bind:key="action.target">
+                            @click="bookmark=action.target"
+                            v-for="action in book[bookmark].actions" v-bind:key="action.target">
                             {{action.text}}
                         </v-btn>
                     </v-card-actions>
@@ -22,29 +22,16 @@
     </v-container>
 </template>
 <script>
+
+//Temporary import for books
+import books from '../assets/books.json'
+
 export default {
     data () {
         return {
             cursor:0,
-            placeholder_book:{
-                'name': 'A Placeholder book',
-                'chapters':[
-                    {'title':'first chapter', 'text':'first chapter text',
-                        'actions':[{'target':'1','text':'chapter 2'}]
-                    },
-                    {'title':'second chapter', 'text':'There should be 2 actions, one leading to 1, one to 3',
-                        'actions':[
-                            {'target':'0','text':'chapter 1'},
-                            {'target':'2','text':'chapter 3'}]
-                    },
-                    {'title':'Third chapter', 'text':'There should be three actions, one leading to 1, one to 2, and one doing nothing',
-                        'actions':[
-                            {'target':'0','text':'chapter 1'},
-                            {'target':'1','text':'chapter 2'},
-                            {'target':'2','text':'Stay here'}]
-                    }
-                ],
-            }
+            book:books.BookOne,
+            bookmark: books.BookOne.start,    
         }
     }
     
