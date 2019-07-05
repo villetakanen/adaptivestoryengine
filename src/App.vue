d229d9541f6e8b242cabec8197d55f1f708fd444
Ville Takanen <ville.takanen@foodie.fm>
2019-07-05 11:44:23 +0300

    Removed HelloWorld from vue create and vuetify, replaced with StoryBoard placehoder.

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,26 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ASE</span>
+        <span class="font-weight-light"> ADAPTIVE STORY ENGINE</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <StoryBoard/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import StoryBoard from './components/StoryBoard'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StoryBoard
   },
   data () {
     return {
---
+++ src/components/StoryBoard.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-card-title>A Chapter goes here</v-card-title>
+        <v-card-text>The Chapter text goes here</v-card-text>
+        <v-card-actions>Here we will have next and previous actions</v-card-actions>
+    </v-card>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
+
+
