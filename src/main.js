2051289f02bb0d94eef784e33df2da8c5d832c61
Ville Takanen <ville.takanen@foodie.fm>
2019-07-08 12:21:11 +0300

    Added vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex)
 
 new Vue({
   render: h => h(App),
